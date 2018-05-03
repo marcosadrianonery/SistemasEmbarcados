ab0652657e760f420f8389c9bccf8d09762b6e7d
marcosadrianonery <30875671+marcosadrianonery@users.noreply.github.com>
2018-05-02 22:00:41 -0300

    Update and rename 2_PCs/Codigos_PC_02 /tirar_foto.c to  2_PCs/ Codigos_PC_02/tirar_foto.c

---
+++ 2_PCs/ Codigos_PC_02/tirar_foto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+int main(int argc, const char * argv[]) {
+int n;
+char entrada[40];
+char string_foto[40];
+		strcpy (entrada, argv[1]);
+		strcat (entrada, " ");		
+		strcat (entrada, argv[2]);
+		//puts(entrada);
+		system(entrada);
+		//sleep(2);
+		strcpy (string_foto, "gpicview");
+		strcat (string_foto, " ");		
+		strcat (string_foto, argv[2]);
+		system(string_foto);	
+}
